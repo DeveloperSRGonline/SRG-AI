38c2f7ac4682d554a8bd82143540ebd82aeaf065
Shivam Garade <shivamgarade05@gmail.com>
2025-11-19 20:07:45 +0530

    feat: integrate AI response generation and message handling
    - Added @google/genai and cookie dependencies to package.json
    - Created message model for MongoDB to store user and AI messages
    - Implemented ai.service.js for generating AI responses using Google GenAI
    - Enhanced socket server to handle user authentication via JWT and manage AI message interactions
    - Saved user messages and AI responses to the database

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,24 +1,75 @@
 // require Server class from socket.io
 const { Server, Socket } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+const aiService = require('../services/ai.service')
+const messageModel = require("../models/message.model");
+const { default: mongoose } = require("mongoose");
+
 
 // socket server init function
 // we pass httpServer because socket.io works on top of http server
 function initSocketServer(httpServer) {
-
   // create new socket server
   // you can pass options later (cors, timeouts, etc.)
   const io = new Server(httpServer, {});
 
+  io.use(async (socket, next) => {
+    // extracting cookie comming from socket
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+    // first we check in cookie token exist of not
+    if (!cookies.token) {
+      next(new Error("Authentication error: no token provided"));
+    }
+
+    // if token available then we verify it
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET_KEY);
+      socket.user = await userModel.findById(decoded.id);
+      next();
+    } catch (error) {
+      next(new Error("Authentication error: invalid token"));
+    }
+  });
+
   // this runs when any user connects
   io.on("connection", (socket) => {
+    socket.on("ai-message", async (messagePayload) => {
 
-    // show socket id in terminal for debugging
-    console.log("New socket connection : ", socket.id);
+      // saving user message in the mongodb
+      await messageModel.create({
+        chat:messagePayload.chat,
+        user:socket.user._id,
+        content:messagePayload.content,
+        role:"user"
+      })
 
-    // here later you will listen events like:
-    // socket.on("send_message", () => {})
-    // socket.on("join_chat", () => {})
-    // etc.
+      // getting chat history of the chat id
+      const chatHistory = await messageModel.find({chat:messagePayload.chat})
+    
+      // getting ai response
+      const response = await aiService(chatHistory.map(item => {
+        return {
+          role:item.role,
+          parts:[{text:item.content}]
+        }
+      }))
+
+      // sending ai response to the frontend
+      socket.emit('ai-response',{
+        content:response,
+        chat:messagePayload.chat
+      })
+
+      // saving model response in the mongodb
+      await messageModel.create({
+        chat:messagePayload.chat,
+        user:socket.user._id,
+        content:response,
+        role:"model"
+      })
+    });
   });
 }
 
